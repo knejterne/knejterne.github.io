3b93d24bb432b30ac0b66ac8e420ea692d630c99
Albert Valentin Raith <albe3306@frbskole.dk>
2019-10-31 11:17:20 +0100

    areal

--- sketch.js
+++ sketch.js
@@ -16,12 +16,105 @@ var areallist=[
             {
                 info: ["Radius"],
                 formula: "pi*Radius*Radius"
-            }
-        ],
-        formulas: [
+            },
             {
                 info: ["Diameter"],
                 formula: "pi*pow(Diameter/2,2)"
+            }
+        ]
+    },
+    {
+        name:"Trapez",
+        formulas: [
+            {
+                info: ["a", "c", "Højde"],
+                formula: ["1/2*(a+c)*Højde"]
+            }
+        ]
+    },
+    {
+        name:"Cirkeludsnit",
+        formulas: [
+            {
+                info: ["Grader", "Radius"],
+                formula: ["1/2*pow(Radius,2)*(Grader/180*pi-sin(Grader/180*pi))"]
+            },{
+                info: ["Radianer", "Radius"],
+                formula: ["1/2*pow(Radius,2)*(Radianer-sin(Radianer))"]
+            }
+        ]
+    },
+    {
+        name:"Regulær polygon",
+        formulas: [
+            {
+                info: ["Sidelængde", "Sider"],
+                formula: ["1/4*Sider*pow(Sidelængde,2)*atan(pi/Sider)"]
+            }
+        ]
+    },
+    {
+        name:"Cirkelring",
+        formulas: [
+            {
+                info: ["Indre radius", "Ydre radius"],
+                formula: ["pi*(pow(Ydre radius,2)-pow(Indre radius,2))"]
+            }
+        ]
+    },
+    {
+        name:"Cirkeludsnit",
+        formulas: [
+            {
+                info: ["Radius", "Radianer"],
+                formula: ["pow(Radius)*Radianer/2"]
+            },
+            {
+                info: ["Radius", "Grader"],
+                formula: ["pow(Radius)*Grader/180*pi/2"]
+            }
+        ]
+    },
+    {
+        name:"Parallelogram",
+        formulas: [
+            {
+                info: ["b", "Højde"],
+                formula: ["b*Højde"]
+            },
+            {
+                info: ["a", "b", "Radianer"],
+                formula: ["a*b*sin(Radianer)"]
+            },
+            {
+                info: ["a", "b", "Grader"],
+                formula: ["a*b*sin(Grader/180*pi)"]
+            }
+        ]
+    },
+    {
+        name:"Rombe",
+        formulas: [
+            {
+                info: ["Diagonal 1", "Diagonal 2"],
+                formula: ["1/2*Diagonal 1*Diagonal 2"]
+            },
+            {
+                info: ["a", "Radianer"],
+                formula: ["sin(Radianer)*pow(a,2)"]
+            },
+            {
+                info: ["a", "b", "Grader"],
+                formula: ["sin(Grader/180*pi)*pow(a,2)"]
+            }
+        ]
+    },
+    {
+        name:"RTrekant",
+        formulas: [
+            {
+                info: ["Højde", "Grundlinje"],
+                formula: ["Højde*Grundlinje/2"]
             }
         ]
     }
